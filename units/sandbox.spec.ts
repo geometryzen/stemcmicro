6aa71b238d1428fddbed245e06768c4729e4f13b
David Geo Holmes <geometryzen@gmail.com>
2023-03-08 15:28:26 -0500

    metrics

--- units/sandbox.spec.ts
+++ units/sandbox.spec.ts
@@ -4,16 +4,17 @@ import { create_script_context } from "../src/runtime/script_engine";
 describe("metrics", function () {
     it("General", function () {
         const lines: string[] = [
-            `G11 = algebra([g11, g22], ["i", "j"])`,
+            `G11 = algebra([g11, g22,g33], ["i", "j","k"])`,
             `e1 = G11[1]`,
             `e2 = G11[2]`,
-            `e2|e1`,
+            `e3 = G11[3]`,
+            `e3|e3`,
         ];
         const engine = create_script_context({
             dependencies: ['Blade']
         });
         const { values } = engine.executeScript(lines.join('\n'));
-        assert.strictEqual(engine.renderAsInfix(values[0]), "0");
+        assert.strictEqual(engine.renderAsInfix(values[0]), "g33");
         engine.release();
     });
 });

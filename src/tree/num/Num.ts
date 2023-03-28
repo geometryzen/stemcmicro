fd1fb3c51b369001f10fe3ecc27284c0a00b2e2d
David Geo Holmes <geometryzen@gmail.com>
2023-03-27 23:42:33 -0400

    cleanup

--- src/tree/num/Num.ts
+++ src/tree/num/Num.ts
@@ -3,7 +3,7 @@ import { Rat } from "../rat/Rat";
 
 /**
  * The Num type is the union of Flt and Rat (and maybe Int in future).
- * While the concept of a number in mathematics may be quite general, in this case we me the
+ * While the concept of a number in mathematics may be quite general, in this case we mean the
  * set of things that we can calculate with. Not surprisingly, these need to be treated as a
  * combined entity so that simlifications can be performed.
  */

b34a27dc4d0f318eb660475c37983f1e995b5493
David Geo Holmes <geometryzen@gmail.com>
2023-12-10 22:15:33 -0500

    typo

--- src/scripting/evaluate_integer.ts
+++ src/scripting/evaluate_integer.ts
@@ -3,7 +3,7 @@ import { nativeInt } from "../nativeInt";
 import { U } from "../tree/tree";
 
 /**
- * Evaluates the expression then converts it ot a JavaScript number.
+ * Evaluates the expression then converts it to a JavaScript number.
  */
 export function evaluate_integer(expr: U, $: ExtensionEnv): number {
     return nativeInt($.valueOf(expr));

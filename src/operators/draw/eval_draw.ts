374b4af40d01570023a39cdbc52936d3480c1ef8
David Geo Holmes <geometryzen@gmail.com>
2024-01-05 21:10:10 -0500

    api

--- src/operators/draw/eval_draw.ts
+++ src/operators/draw/eval_draw.ts
@@ -3,5 +3,9 @@ import { ExtensionEnv } from '../../env/ExtensionEnv';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function Eval_draw(expr: Cons, $: ExtensionEnv): U {
+    const F = $.valueOf(expr.item(1));
+    const X = $.valueOf(expr.item(2));
+    // eslint-disable-next-line no-console
+    console.log(`draw(${$.toInfixString(F)},${$.toInfixString(X)})`);
     return nil;
 }

eb6315edaf2a2c9261829c538d4b6b0504f2ae9d
David Geo Holmes <geometryzen@gmail.com>
2024-01-30 13:22:46 -0500

    'Diagnostics'

--- src/env/env.ts
+++ src/env/env.ts
@@ -1250,6 +1250,7 @@ export function create_env(options?: EnvOptions): ExtensionEnv {
                             return [TFLAG_DIFF, newExpr];
                         }
                         else {
+                            // eslint-disable-next-line no-console
                             console.warn(`${head}`);
                             throw new ProgrammingError();
                         }

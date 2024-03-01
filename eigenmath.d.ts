0687e0a26fd063aee43061fc589125baca4aa0fd
David Geo Holmes <geometryzen@gmail.com>
2024-03-01 13:45:01 -0500

    'eigenmath.d.ts'

--- eigenmath.d.ts
+++ eigenmath.d.ts
@@ -25,62 +25,62 @@ type U = any
 //==============================================================================
 
 /**
- * @returns the absolute value or vector length of `x`.
+ * Returns the absolute value or vector length of `x`.
  */
 declare function abs(x: U): U
 
 /**
- * @returns the adjunt of marix `m`, Adjunt is equal to determinant times inverse.
+ * Returns the adjunt of marix `m`, Adjunt is equal to determinant times inverse.
  */
 declare function adj(m: U): U
 
 /**
- * @returns true if all arguments are true. Returns false otherwise.
+ * Returns `true` if all arguments are `true`. Returns `false` otherwise.
  */
-declare function and(...a: U[]): U
+declare function and(...args: U[]): U
 
 /**
- * @returns the arc cosine of `x`.
+ * Returns the arc cosine of `x`.
  */
 declare function arccos(x: U): U
 
 /**
- * @returns the arc hyperbolic cosine of `x`.
+ * Returns the arc hyperbolic cosine of `x`.
  */
 declare function arccosh(x: U): U
 
 /**
- * @returns the arc sine of `x`.
+ * Returns the arc sine of `x`.
  */
 declare function arcsin(x: U): U
 
 /**
- * @returns the arc hyperbolic sine of `x`.
+ * Returns the arc hyperbolic sine of `x`.
  */
 declare function arcsinh(x: U): U
 
 /**
- * @returns the arc tangent of `y` over `x`. If `x` is omitted then `x`=1 is used.
+ * Returns the arc tangent of `y` over `x`. If `x` is omitted then `x`=1 is used.
  */
 declare function arctan(y: U, x?: U): U
 
 /**
- * @returns the arc hyperbolic tangent of `x`.
+ * Returns the arc hyperbolic tangent of `x`.
  */
 declare function arctanh(x: U): U
 
 /**
- * @returns the angle of complex `z`.
+ * Returns the angle of complex `z`.
  */
 declare function arg(z: U): U
 
 /**
- * @returns the binding of the symbol `s` without evaluation.
+ * Returns the binding of the symbol `s` without evaluation.
  */
 declare function binding(s: U): U
 
 /**
- * @returns the smallest integer greater than or equal to `x`.
+ * Returns the smallest integer greater than or equal to `x`.
  */
 declare function ceiling(x: U): U
 
@@ -90,66 +90,333 @@ declare function ceiling(x: U): U
 declare function check(x: U): U
 
 /**
- * @returns the binomial coefficient `n` choose `k`.
+ * Returns the binomial coefficient `n` choose `k`.
  */
 declare function choose(n: U, k: U): U
 
 /**
- * @returns the expression `x` with circular and hyperbolic function converted to exponentials.
+ * Returns the expression `x` with circular and hyperbolic function converted to exponentials.
  */
 declare function circexp(x: U): U
 
 /**
- * @returns complex `z` in polar form with base of negative one instead of `e`.
+ * Returns complex `z` in polar form with base of negative one instead of `e`.
  */
 declare function clock(z: U): U
 
 /**
- * @returns the cofactor of matrix `m` for row `i` and column `j`.
+ * Returns the cofactor of matrix `m` for row `i` and column `j`.
  */
 declare function cofactor(m: U, i: U, j: U): U
 
 /**
- * @returns the complex conjugate of `z`.
+ * Returns the complex conjugate of `z`.
  */
 declare function conj(z: U): U
 
 /**
- * @returns tensor `a` summed over indeices `i` and `j`. If `i` and `j` are omitted then `1` and `2` are used.
+ * Returns tensor `a` summed over indeices `i` and `j`. If `i` and `j` are omitted then `1` and `2` are used.
  */
 declare function contract(a: U, i: U, j: U): U
 
 /**
- * @returns the cosine of `x`.
+ * Returns the cosine of `x`.
  */
 declare function cos(x: U): U
 
 /**
- * @returns the hyperbolic cosine of `x`.
+ * Returns the hyperbolic cosine of `x`.
  */
 declare function cosh(x: U): U
 
 /**
- * @returns the cross product of vectors `u` and `v`.
+ * Returns the cross product of vectors `u` and `v`.
  */
 declare function cross(u: U, v: U): U
 
 /**
- * @returns the hyperbolic cosine of `x`.
+ * Returns the hyperbolic cosine of `x`.
  */
 declare function curl(v: U): U
 
 /**
- * @returns the definite integral of `f` with respect to `x` evaluated from `a` to `b`.
+ * Returns the definite integral of `f` with respect to `x` evaluated from `a` to `b`.
  */
 declare function defint(f: U, x: U, a: U, b: U): U
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+/**
+ * Returns the denominator of expression `x`.
+ */
+declare function denominator(x: U): U
 
 /**
- * @returns the square root of `x`.
+ * Returns the determinant of matrix `m`.
+ */
+declare function det(m: U): U
+
+/**
+ * Returns the dimension of the `n`th index of tensor `a`.
+ */
+declare function dim(a: U, n: U): U
+
+/**
+ * Returns the divergence of vector `v` with respect to the symbols `x`, `y`, and `z`.
+ */
+declare function div(v: U): U
+
+// TODO: do is a problem because of name conflict in JavaScript.
+// declare function do(...args: U[]): U
+
+/**
+ * Returns the dot product of vectors, matrices, and tensors.
+ */
+declare function dot(...arg: U[]): U
+
+/**
+ * Drasw the graph of `f`(`x`). Drawing ranges can be set with `xrange` and `yrange`.
+ */
+declare function draw(f: U, x: U): U
+
+/**
+ * Returns the eigenvectors for matrix `m`. Matrix `m` is required to be numerical, real, and symmetric.
+ * The return value is a matrix with each column an eigenvector.
+ */
+declare function eigenvec(m: U): U
+
+/**
+ * Returns `f` evaluated with `x` replaced by `a`, etc. All arguments can be expressions.
+ */
+declare function eval(f: U, x: U, a: U, ...args: U[]): U
+
+/**
+ * Returns the exponential of `x`.
+ */
+declare function exp(x: U): U
+
+/**
+ * Returns the cosine of `z` in exponential form.
+ */
+declare function expcos(z: U): U
+
+/**
+ * Returns the hyperbolic cosine of `z` in exponential form.
+ */
+declare function expcosh(z: U): U
+
+/**
+ * Returns the sine of `z` in exponential form.
+ */
+declare function expsin(z: U): U
+
+/**
+ * Returns the hyperbolic sine of `z` in exponential form.
+ */
+declare function expsinh(z: U): U
+
+/**
+ * Returns the tangent of `z` in exponential form.
+ */
+declare function exptan(z: U): U
+
+/**
+ * Returns the hyperbolic tangent of `z` in exponential form.
+ */
+declare function exptanh(z: U): U
+
+/**
+ * Returns the factorial of `n`. The expression `n!` can also be used.
+ */
+declare function factorial(n: U): U
+
+/**
+ * Returns expression `x` with rational numbers and integers converted to floating point values.
+ */
+declare function float(x: U): U
+
+// declare function for(x: U): U
+
+/**
+ * Returns the gradient `derivative(f, [x, y, z])`.
+ */
+declare function grad(f: U): U
+
+/**
+ * Returns the Hadamard (element-wise) product. The arguments are required to have the same dimensions.
+ */
+declare function hadamard(...args: U[]): U
+
+/**
+ * Returns the imaginary part of complex `z`.
+ */
+declare function imag(z: U): U
+
+/**
+ * Returns the expression `x` as a string in `infix` format.
+ */
+declare function infix(x: U): U
+
+/**
+ * Returns the inner product of vectors, matrices, and tensors.
+ */
+declare function inner(...args: U[]): U
+
+/**
+ * Returns the integral of `f` with respect to `x`.
+ */
+declare function integral(f: U, x: U): U
+
+/**
+ * Returns the inverse of matrix `m`.
+ */
+declare function inv(m: U): U
+
+/**
+ * Returns the kronecker product of vectors and matrices.
+ */
+declare function kronecker(...args: U[]): U
+
+/**
+ * Returns the natural logarithm of `x`.
+ */
+declare function log(x: U): U
+
+/**
+ * Returns the magnitude of complex `z`.
+ */
+declare function mag(z: U): U
+
+/**
+ * Returns the minor of matrix `m` for row `i` and column `j`.
+ */
+declare function minor(m: U, i: U, j: U): U
+
+/**
+ * Returns a copy of matrix `m` with row `i` and column `j` removed.
+ */
+declare function minormatrix(m: U, i: U, j: U): U
+
+/**
+ * Returns the remainder of `a` divided by `b`.
+ */
+declare function mod(a: U, b: U): U
+
+/**
+ * Evaluates expression `x` without expanding products of sums.
+ */
+declare function noexpand(x: U): U
+
+/**
+ * Returns `false` if `x` is `true`. Returns `true` otherwise.
+ */
+declare function not(x: U): U
+
+/**
+ * Returns the approximate roots of polynomial `p` in variable `x` with real or complex coefficients.
+ */
+declare function nroots(p: U, x: U): U
+
+/**
+ * Returns the numerator of expression `x`.
+ */
+declare function numerator(x: U): U
+
+/**
+ * Returns `true` if at least one argument is `true`. Returns `false` otherwise.
+ */
+declare function or(...args: U[]): U
+
+/**
+ * Returns the outer product of vectors, matrices, and tensors.
+ */
+declare function outer(...args: U[]): U
+
+/**
+ * Returns the complex `z` in polar form.
+ */
+declare function polar(z: U): U
+
+/**
+ * Returns the number of indices for tensor `a`.
+ */
+declare function rank(a: U): U
+
+/**
+ * Returns the expression `x` with everything over a common denominator.
+ */
+declare function rationalize(x: U): U
+
+/**
+ * Returns the real part of complex `z`.
+ */
+declare function real(z: U): U
+
+/**
+ * Returns complex `z` in rectangular form.
+ */
+declare function rect(z: U): U
+
+/**
+ * Returns the rational roots of polynomial `p` in variable `x`.
+ */
+declare function roots(p: U, x: U): U
+
+/**
+ * Returns the vector `u` rotated according to to rotation codes.
+ */
+declare function rotate(u: U, ...codes: U): U
+
+/**
+ * Returns the expression `x` in a simpler form.
+ */
+declare function simplify(x: U): U
+
+/**
+ * Returns the sine of `x`.
+ */
+declare function sin(x: U): U
+
+/**
+ * Returns the hyperbolic sine of `x`.
+ */
+declare function sinh(x: U): U
+
+/**
+ * Returns the square root of `x`.
  */
 declare function sqrt(x: U): U
+
+/**
+ * Returns the tangent of `x`.
+ */
+declare function tan(x: U): U
+
+/**
+ * Returns the hyperbolic tangent of `x`.
+ */
+declare function tanh(x: U): U
+
+/**
+ * Returns the `n`th order Taylor series expansion of `f`(`x`) at `a`.
+ */
+declare function taylor(f: U, x: U, n: U, a: U): U
+
+// declare function test(...args: U[]): U
+
+/**
+ * Returns the transpose of tensor `a` with respect to indices `i` and `j`.
+ * If `i` and `j` are omitted then `1` and `2` are used.
+ */
+declare function transpose(a: U, i?: U, j?: U): U
+
+/**
+ * Returns an `n` by `n` identity matrix.
+ */
+declare function unit(n: U): U
+
+/**
+ * Returns a null tensor with dimensions `i`, `j`, etc.
+ */
+declare function zero(...dims: U[]): U
 
 //==============================================================================
 // Algebras
@@ -362,3 +629,12 @@ declare const peta: Sym;
  * Prefix indicating one quintillion (10**18)
  */
 declare const exa: Sym;
+
+//==============================================================================
+// Constants
+//==============================================================================
+
+/**
+ * Mathematical constant, Pi
+ */
+declare const pi: Sym;

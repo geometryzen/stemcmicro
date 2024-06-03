2b6f704ed30e2597c777278e43f297e8cea300cb
David George Holmes <geometryzen@gmail.com>
2024-06-03 11:43:48 -0400

    Updates

---
+++ hilbert/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslLKyMxJSi0qUdJRKkgsyVCyUkorzUsuyczPK9aHSulllOTmKOkoZWfmpShZmZnU6qBr9knMTUpJdK0oKMJjDEIRhoGxAGtKesCPAAAA"
